3de62cd0ff3ddc455f73adc4ac8d2d7a8b3a13d9
Hiago De Franco <hfranco@baylibre.com>
2026-04-23 13:25:15 -0300

    WIP: add milkv-duo-256m
    Signed-off-by: Hiago De Franco <hfranco@baylibre.com>

---
+++ board/sophgo/milkv_duo_256m/board.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2026, <hfranco@baylibre.com>
+ */
+
+#include <dm/lists.h>
+#include "ethernet.h"
+
+int board_init(void)
+{
+	if (IS_ENABLED(CONFIG_SYSRESET_CV1800B))
+		device_bind_driver(gd->dm_root, "cv1800b_sysreset", "sysreset", NULL);
+
+	if (IS_ENABLED(CONFIG_NET_LEGACY))
+		cv1800b_ephy_init();
+
+	return 0;
+}
---
+++ board/sophgo/milkv_duo_256m/ethernet.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2026, <hfranco@baylibre.com>
+ */
+
+#ifndef __CV1800B_ETHERNET_H
+#define __CV1800B_ETHERNET_H
+
+void cv1800b_ephy_init(void);
+
+#endif
